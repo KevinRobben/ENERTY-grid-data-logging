0fc3f80bc64165b59cffdc4995dda87325883ba0
KevinRobben <kevinrobben1999@gmail.com>
2025-01-09 17:36:17 +0100

    Add startup delay and update functionality to gridconnection_watcher.py

--- gridconnection_watcher.py
+++ gridconnection_watcher.py
@@ -4,8 +4,8 @@ import logging
 import time
 from module_m_decoder import ModuleM
 
-
 logging.info("starting gridconnection_watcher.py")
+time.sleep(5) # sleep 5 seconds at startup. If we crash, we overload systemD with too many restarts when not sleeping
 
 module_m = ModuleM()
 
@@ -16,13 +16,14 @@ def update():
 
 def update_from_github():
     logging.info("updating gridconnection_watcher.py")
+    os.system("sudo git reset --hard")
     os.system("sudo git pull")
     os.system("sudo systemctl restart gridconnection_watcher.service")
 
 
 
 if __name__ == "__main__":
-    time.sleep(5) # sleep 5 seconds at startup. If we crash, we overload systemD with too many restarts when not sleeping
+    
     # Configure logging to send messages to stdout (captured by systemd)
     logging.basicConfig(level=logging.INFO, format='%(message)s')
     
