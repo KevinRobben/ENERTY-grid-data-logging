65ea88961c597994d44ab9cf171634a5901faa23
KevinRobben <kevinrobben1999@gmail.com>
2025-01-11 19:04:21 +0100

    Add script to activate virtual environment for project setup

---
+++ venv_acitvate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to the directory containing the virtual environment
+cd /home/kevin/ENERTY-grid-data-logging
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Optional: Run a Python script or commands within the environment
+echo "Virtual environment activated!"
+# python3 your_script.py
+
+# Uncomment the following line to keep the terminal open after activation
+exec bash
